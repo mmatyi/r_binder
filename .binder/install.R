41b1d94a485a0c4cc27a0188316655e97a431c6f
Mészáros Mátyás <34501282+mmatyi@users.noreply.github.com>
2024-03-26 20:42:24 +0100

    remove packages

--- .binder/install.R
+++ .binder/install.R
@@ -1,5 +1,5 @@
-install.packages(c("tidyr", "dplyr","reshape2","tidyverse","giscoR","tmap","highcharter","plotly","RJDemetra"), repos="https://cloud.r-project.org")
-install.packages(c("ggplot2","ggforce","kableExtra","bookdown","rmarkdown"),repos="https://cloud.r-project.org")
+install.packages(c("reshape2","tidyverse","giscoR","tmap","RJDemetra"), repos="https://cloud.r-project.org")
+install.packages(c("ggplot2"),repos="https://cloud.r-project.org")
 
 # Eurostat package restatapi: we get the dev version
 remotes::install_github("eurostat/restatapi")

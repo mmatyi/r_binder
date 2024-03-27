33c0061bed4b4a4182cf1d81747154ccc7948257
Mészáros Mátyás <34501282+mmatyi@users.noreply.github.com>
2024-03-27 08:46:41 +0100

    adding back RJDemetra

--- .binder/install.R
+++ .binder/install.R
@@ -1,4 +1,4 @@
-install.packages(c("reshape2","tidyverse","giscoR","tmap"), repos="https://cloud.r-project.org")
+install.packages(c("reshape2","tidyverse","giscoR","tmap","RJDemetra"), repos="https://cloud.r-project.org")
 install.packages(c("ggplot2"),repos="https://cloud.r-project.org")
 
 # Eurostat package restatapi: we get the dev version
